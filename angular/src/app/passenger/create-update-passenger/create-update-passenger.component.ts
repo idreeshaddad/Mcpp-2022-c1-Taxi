65087461018b8cd03dc528cbfe75b64a1a6da94e
Idrees Haddad <gelnox@live.com>
2023-07-31 18:49:39 +0300

    Finished Passenger pages

--- angular/src/app/passenger/create-update-passenger/create-update-passenger.component.ts
+++ angular/src/app/passenger/create-update-passenger/create-update-passenger.component.ts
@@ -2,7 +2,7 @@ import { ConfirmationService } from '@abp/ng.theme.shared';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CreateUpdatePassengerDto, PassengerDetailsDto, PassengerService } from '@proxy/passengers';
 
 @Component({
@@ -12,19 +12,40 @@ import { CreateUpdatePassengerDto, PassengerDetailsDto, PassengerService } from 
 })
 export class CreateUpdatePassengerComponent implements OnInit {
 
-  passenger: CreateUpdatePassengerDto;
   passengerForm: FormGroup;
+  passengerId: number = 0;
 
   constructor(
     private fb: FormBuilder,
     private passengerSvc: PassengerService,
     private router: Router,
-    private confirmation: ConfirmationService
+    private confirmation: ConfirmationService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
 
+    this.setPassengerId();
+
     this.buildForm();
+
+    if (this.passengerId != 0) {
+
+      this.loadPassenger();
+    }
+
+  }
+
+  loadPassenger() {
+
+    this.passengerSvc.get(this.passengerId).subscribe({
+      next: (passengerFromApi: PassengerDetailsDto) => {
+        this.passengerForm.patchValue(passengerFromApi);
+      },
+      error: (err: HttpErrorResponse) => {
+        this.confirmation.error(err.message, err.name, { hideYesBtn: true, cancelText: 'Ok' });
+      }
+    });
   }
 
   buildForm() {
@@ -37,18 +58,34 @@ export class CreateUpdatePassengerComponent implements OnInit {
 
   }
 
-  createPassenger(): void {
+  setPassengerId(): void {
+
+    if (this.activatedRoute.snapshot.paramMap.get('id')) {
+      this.passengerId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    }
+  }
+
+  submitForm(): void {
 
     if (this.passengerForm.valid) {
 
-      this.passengerSvc.create(this.passengerForm.value).subscribe({
-        next: (passengerDetailsFromApi: PassengerDetailsDto) => {
-          this.router.navigate(['/passengers']);
-        },
-        error: (err: HttpErrorResponse) => {
-          this.confirmation.error(err.message, err.name, { hideYesBtn: true, cancelText: 'Ok' });
-        }
-      });
+      if (this.passengerId == 0) {
+        this.passengerSvc.create(this.passengerForm.value).subscribe({
+          next: (passengerDetailsFromApi: PassengerDetailsDto) => {
+            this.router.navigate(['/passengers']);
+          }
+        });
+      }
+      else {
+
+        this.passengerSvc.update(this.passengerId, this.passengerForm.value).subscribe({
+          next: (passengerDetailsFromApi: PassengerDetailsDto) => {
+            this.router.navigate(['/passengers']);
+          }
+        });
+      }
+
     }
   }
 }
+
